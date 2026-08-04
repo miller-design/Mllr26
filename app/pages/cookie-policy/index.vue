e10a4186c1a4775811b144665734618c530b0554
Jack <jack.miller@live.co.uk>
2026-08-04 22:38:57 +0100

    Privacy and Cookie policy routes added

---
+++ app/pages/cookie-policy/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+/**
+ * Privacy policy page.
+ */
+</script>
+
+<template>
+  <div>
+    <h1>Cookie Policy</h1>
+  </div>
+</template>
---
+++ app/pages/privacy-policy/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+/**
+ * Privacy policy page.
+ */
+</script>
+
+<template>
+  <div>
+    <h1>Privacy Policy</h1>
+  </div>
+</template>
